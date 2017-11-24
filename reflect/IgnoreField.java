0658ad78cdf12cc8cc7c9492dc83c411a3bb60ed
ace1981 <38688368@qq.com>
2017-11-24 14:02:26 -0600

    Add files via upload

---
+++ reflect/IgnoreField.java
@@ -0,0 +1,10 @@
+package cn.dadaclub.reflect;
+
+import java.lang.annotation.*;
+
+
+@Retention(RetentionPolicy.RUNTIME) // 注解会在class字节码文件中存在，在运行时可以通过反射获取到  
+@Target({ElementType.FIELD})//定义注解的作用目标**作用范围字段、枚举的常量/方法  
+public @interface IgnoreField {
+
+}
